f6a3b8411032d520c67ce2e19fe1e8320974be0a
scohenma <sabrinacm03@gmail.com>
2025-02-09 18:46:35 -0500

    Streamlit App Filters - Progress 1

--- basic-streamlit-app/main.py
+++ basic-streamlit-app/main.py
@@ -5,11 +5,12 @@ import pandas as pd
 st.title("Palmer's Penguins Interactive App")
 st.write("Explore the Palmer's Penguins dataset with interactive filters! Discover details about each penguin, from its species to its island habitat, and dive into the data with ease")
 
+st.write("Below is the complete dataset loaded from a CSV File:")
 df = pd.read_csv("data/penguins.csv")
 st.dataframe(df)
 
 island = st.radio("Select an Island:", ["All"] + list(df["island"].unique()))
 gender = st.selectbox("Select Gender:", ["All", "Male", "Female"])
-
+species_selected = st.multiselect("Select Species:", df["species"].unique(), default=df["species"].unique())
 
 
