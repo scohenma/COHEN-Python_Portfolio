c8a51d630d2d6231301de2b2ef0a816dd20d8927
scohenma <sabrinacm03@gmail.com>
2025-04-14 15:35:35 -0400

    Update app.py

--- NERStreamlitAPP/app.py
+++ NERStreamlitAPP/app.py
@@ -10,7 +10,7 @@ nlp = spacy.load('en_core_web_sm')
 
 #Set the title
 st.title("Custom NER Interactive App")
-st.write("This app allows you to explore custom Named Entity Recognition using SpaCy by uploading and exploring your own text!")
+st.write("This app lets you explore custom Named Entity Recognition (NER) using spaCy. Upload or paste your own text, define your own entity rules, and see how NER highlights the people, places, and things in your writing!")
 
 #User inputs
 # Step 1: Upload or paste your text
@@ -27,11 +27,11 @@ sample_choice = st.selectbox("Choose a sample or write your own:", sample_option
 
 if sample_choice == "Sample 1: Stock market volatility":
     st.caption("Try label: PERSON and pattern: Trump")
-    text = "It’s been a week of uncertainty and volatility in the stock market as former President Trump introduces new tariff policies impacting global trade."
+    text = "It’s been a week of uncertainty and volatility in the stock market as  President Trump introduces new tariff policies impacting global trade."
 
 elif sample_choice == "Sample 2: Katy Perry's space launch":
     st.caption("Try label: PERSON and pattern: Katy Perry")
-    text = "Katy Perry joined the crew of Blue Origin's latest space mission, which launched and landed successfully earlier this week."
+    text = "Katy Perry joined the all-female crew of Blue Origin's latest space mission, which launched and landed successfully earlier this week."
 
 elif sample_choice == "Sample 3: Notre Dame in Forbes ranking":
     st.caption("Try label: ORG and pattern: University of Notre Dame")
