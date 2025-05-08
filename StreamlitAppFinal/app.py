dd2b33b2a1d582a7f31d15f67bfe6d1bbfc74f56
scohenma <sabrinacm03@gmail.com>
2025-05-08 18:33:20 -0400

    Update app.py

--- StreamlitAppFinal/app.py
+++ StreamlitAppFinal/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 # Load data
-df = pd.read_csv('universities_final_.csv')
+df = pd.read_csv('universities_final_2.csv')
 df.columns = df.columns.str.strip()
 st.write("📌 Column names:", df.columns.tolist())
 
